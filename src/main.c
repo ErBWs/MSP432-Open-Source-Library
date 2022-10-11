2af194d375cd650872654fd192088f6d631de59a
ErBW_s <erbws@foxmail.com>
2022-10-11 11:48:26 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ int main(void)
     MotorInit();
     GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN1);
 //    EnableInterrupt_TimerA3_0();
-    // EnableTimerInterrupt_ms(TIM32_1_INT, 1000);
-    // EnableTimerInterrupt_ms(TIM_A3_INT, 10);
+     EnableTimerInterrupt_ms(TIM32_1_INT, 1000);
+     EnableTimerInterrupt_ms(TIM_A3_INT, 10);
 
     // Enable global interrupt
     Interrupt_enableMaster();
@@ -22,7 +22,5 @@ int main(void)
     {
         GPIO_setOutputHighOnPin(GPIO_PORT_P6, GPIO_PIN6);
         GPIO_setOutputHighOnPin(GPIO_PORT_P6, GPIO_PIN7);
-        GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN1);
-        Delay_ms(1000);
     }
 }
